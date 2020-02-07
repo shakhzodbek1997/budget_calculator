fc0c824ee90f0a5b8c1decd4359412fe4f768ffb
Shakhzodbek Yuldoshev <shakhzodbek.yuldoshev@gmail.com>
2020-02-07 12:53:14 +0500

    input

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,13 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {MdSend} from 'react-icons/md';
 
-class ExpenseForm extends Component {
-    render() {
-        return (
-            <div>
-                Hello from ExpenseForm
+const ExpenseForm = () => {
+    return <form>
+        <div className="form-center">
+            <div className="form-group">
+                <label htmlFor="charge"> charge</label>
+                <input type="text"
+                       className="form-control"
+                       id="charge"
+                       name="charge"
+                       placeholder=" Example rent..."
+                />
             </div>
-        );
-    }
-}
+            <div className="form-group">
+                <label htmlFor="amount"> amount</label>
+                <input type="text"
+                       className="form-control"
+                       id="amount"
+                       name="amount"
+                       placeholder=" Example 100"
+                />
+            </div>
+        </div>
+    </form>;
+};
 
 export default ExpenseForm;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {MdEdit, MdDelete} from 'react-icons/md';
 
-class ExpenseItem extends Component {
-    render() {
-        return (
-            <div>
-                Hello from ExpenseItem
-            </div>
-        );
-    }
-}
+const ExpenseItem = ({expense}) => {
+    const {id, charge, amount} = expense;
+    return <li className="item">
+        <div className="info">
+            <span className="expense">{charge}</span>
+            <span className="amount">${amount}</span>
+        </div>
+        <div>
+            <button className="edit-btn" aria-label="Edit button">
+                <MdEdit/>
+            </button>
+            <button className="clear-btn" aria-label=" Delete button">
+                <MdDelete/>
+            </button>
+        </div>
+    </li>;
+};
 
 export default ExpenseItem;
